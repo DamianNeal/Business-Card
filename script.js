802a83b290267dd46a393378a2848661785c60bf
Damian Neal Hattingh <136974102+DamianNeal@users.noreply.github.com>
2024-10-29 17:46:29 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function flip() {
+    document.querySelector('.flipper').classList.add('flipped');
+}
+
+function flipBack() {
+    document.querySelector('.flipper').classList.remove('flipped');
+
+}
